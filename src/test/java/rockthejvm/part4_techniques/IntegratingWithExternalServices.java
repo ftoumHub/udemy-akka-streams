220319eff285f19dcba52c923cb38e920a656f4c
ftoum <georgesginon@gmail.com>
2020-05-28 16:43:05 +0200

    testing streams

--- src/test/java/rockthejvm/part4_techniques/IntegratingWithExternalServices.java
+++ src/test/java/rockthejvm/part4_techniques/IntegratingWithExternalServices.java
@@ -1,15 +1,8 @@
 package rockthejvm.part4_techniques;
 
-import static io.vavr.API.println;
-
-import java.util.Date;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-
 import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
-import akka.dispatch.MessageDispatcher;
 import akka.stream.ActorAttributes;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Sink;
@@ -17,14 +10,48 @@ import akka.stream.javadsl.Source;
 import io.vavr.collection.HashMap;
 import io.vavr.collection.List;
 import io.vavr.collection.Map;
-import io.vavr.concurrent.Future;
 import lombok.AllArgsConstructor;
 
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import static io.vavr.API.println;
+import static io.vavr.concurrent.Future.ofSupplier;
+
 public class IntegratingWithExternalServices {
+
+    private static final ActorSystem system = ActorSystem.create("IntegratingWithExternalServices");
+    //final MessageDispatcher dispatcher = system.dispatchers().lookup("dedicated-dispatcher");
+    private static final ActorMaterializer mat = ActorMaterializer.create(system);
 
     // example: simplified PagerDuty
     @AllArgsConstructor
     class PagerEvent{ String application; String description; Date date; }
+
+    static class PagerService {
+        private List<String> engineers = List.of("Daniel", "John", "Lady Gaga");
+        private Map<String, String> emails = HashMap.of(
+                "Daniel", "daniel@rockthejvm.com",
+                "John", "john@rockthejvm.com",
+                "Lady Gaga", "ladygaga@rtjvm.com");
+
+        public CompletableFuture<String> processEvent(PagerEvent pagerEvent) {
+            return ofSupplier(() -> {
+                final Long engineerIndex = (pagerEvent.date.toInstant().getEpochSecond() / (24 * 3600)) % engineers.length();
+                final String engineer = engineers.get(engineerIndex.intValue());
+                final String engineerEmail = emails.get(engineer).getOrNull();
+
+                println("Sending engineer " + engineerEmail + " a high priority notification: " + pagerEvent);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return engineerEmail;
+            }).toCompletableFuture();
+        }
+    }
 
     final Source<PagerEvent, NotUsed> eventSource = Source.from(List.of(
             new PagerEvent("AkkaInfra", "Infrastructure broke", new Date()),
@@ -35,36 +62,8 @@ public class IntegratingWithExternalServices {
 
     public static void main(String[] args) {
 
-        ActorSystem system = ActorSystem.create("IntegratingWithExternalServices");
-        //final MessageDispatcher dispatcher = system.dispatchers().lookup("dedicated-dispatcher");
-        ActorMaterializer mat = ActorMaterializer.create(system);
+        final Source<PagerEvent, NotUsed> infraEvents = new IntegratingWithExternalServices().eventSource.filter(__ -> __.application.equals("AkkaInfra"));
 
-        class PagerService {
-            private List<String> engineers = List.of("Daniel", "John", "Lady Gaga");
-            private Map<String, String> emails = HashMap.of(
-                    "Daniel", "daniel@rockthejvm.com",
-                    "John", "john@rockthejvm.com",
-                    "Lady Gaga", "ladygaga@rtjvm.com");
-
-            public CompletableFuture<String> processEvent(PagerEvent pagerEvent) {
-                return Future.ofSupplier(() -> {
-                    final Long engineerIndex = (pagerEvent.date.toInstant().getEpochSecond() / (24 * 3600)) % engineers.length();
-                    final String engineer = engineers.get(engineerIndex.intValue());
-                    final String engineerEmail = emails.get(engineer).getOrNull();
-
-                    println("Sending engineer " + engineerEmail + " a high priority notification: " + pagerEvent);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    return engineerEmail;
-                }).toCompletableFuture();
-            }
-        }
-
-        final Source<PagerEvent, NotUsed> infraEvents = new IntegratingWithExternalServices()
-                .eventSource.filter(__ -> __.application.equals("AkkaInfra"));
         final Source<String, NotUsed> pagedEngineersEmails =
                 infraEvents.mapAsync(4, event -> new PagerService().processEvent(event));
         // guarantees the relative order of elements
--- src/test/java/rockthejvm/part4_techniques/TestingStreamsSpec.java
+++ src/test/java/rockthejvm/part4_techniques/TestingStreamsSpec.java
@@ -1,10 +1,22 @@
 package rockthejvm.part4_techniques;
 
+import static io.vavr.API.*;
+import static io.vavr.Patterns.$Failure;
+import static io.vavr.Patterns.$Success;
+import static io.vavr.concurrent.Future.fromCompletableFuture;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static scala.compat.java8.FutureConverters.*;
 
 import java.util.concurrent.CompletionStage;
 
+import akka.Done;
+import akka.japi.Pair;
+import akka.stream.testkit.TestPublisher;
+import akka.stream.testkit.TestSubscriber;
+import akka.stream.testkit.TestSubscriber.Probe;
+import akka.stream.testkit.javadsl.TestSink;
+import akka.stream.testkit.javadsl.TestSource;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -23,7 +35,7 @@ public class TestingStreamsSpec {
 
     static ActorSystem system;
     static Materializer mat;
-    static TestKit probe;
+    static TestKit probe; // acteur de test
 
     @BeforeClass
     public static void setup() {
@@ -39,25 +51,26 @@ public class TestingStreamsSpec {
     }
 
     @Test
-    public void simpleStream() throws Exception {
-
+    public void simpleStream() {
         final Source<Integer, NotUsed> simpleSource = Source.range(0, 10);
 
         // Ce sink va additionner les valeurs qui lui sont passé.
         // Lorsqu'il est matérialisé, il expose son resultat sous forme d'un CompletionStage de Integer
         final Sink<Integer, CompletionStage<Integer>> simpleSink = Sink.fold(0, (agg, next) -> agg + next);
 
-        final CompletionStage<Integer> run = simpleSource.log("SimpleSource")
-                .toMat(simpleSink, Keep.right()).run(mat);
-        int result = run.toCompletableFuture().join();
+        int result = simpleSource.log("SimpleSource")
+                .toMat(simpleSink, Keep.right()).run(mat)
+                .toCompletableFuture().join();
         assertEquals(55, result);
     }
 
     @Test
     public void integrateWithTestActorsViaMaterializedValues() {
-        probe = new TestKit(system); // Acteur spécial avec des capacités de test
+
         final Source<Integer, NotUsed> simpleSource = Source.range(0, 10);
         final Sink<Integer, CompletionStage<Integer>> simpleSink = Sink.fold(0, (agg, next) -> agg + next);
+
+        probe = new TestKit(system); // Acteur spécial avec des capacités de test
 
         final CompletionStage<Integer> future = simpleSource.toMat(simpleSink, Keep.right()).run(mat);
 
@@ -78,5 +91,66 @@ public class TestingStreamsSpec {
                 .log("StreamUnderTest")
                 .to(probeSink).run(mat);
         probe.expectMsgAllOf(0, 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55);
+    }
+
+    @Test
+    public void integrateWithStreamsTestKitSink() {
+        Source<Integer, NotUsed> sourceUnderTest = Source.range(1, 5).map(__ -> __ * 2);
+
+        Sink<Integer, Probe<Integer>> testSink = TestSink.probe(system);
+        Probe<Integer> materializedTestValue = sourceUnderTest.runWith(testSink, mat);
+
+        materializedTestValue
+                .request(5) // si on met plus ou moins d'éléments que prévu dans la source ça pète
+                .expectNext(2, 4, 6, 8, 10)
+                .expectComplete();
+    }
+
+    @Test
+    public void integrateWithStreamsTestKitSource() {
+        Sink<Integer, CompletionStage<Done>> sinkUnderTest = Sink.<Integer>foreach(i ->
+                Match(i).of(
+                        Case($(13), __ -> new RuntimeException("bad luck!")),
+                        Case($(), __ -> __)
+                )
+        );
+
+        Source<Integer, TestPublisher.Probe<Integer>> testSource = TestSource.probe(system);
+        Pair<TestPublisher.Probe<Integer>, CompletionStage<Done>> materialized = testSource.toMat(sinkUnderTest, Keep.both()).run(mat);
+
+        materialized.first()
+                .sendNext(1)
+                .sendNext(5)
+                .sendNext(13)
+                .sendComplete();
+
+        fromCompletableFuture(materialized.second().toCompletableFuture()).onComplete(__ ->
+                Match(__).of(
+                        Case($Success($()), ___ -> run(() -> fail("the sink under test should have thrown an exception on 13"))),
+                        Case($Failure($()), ___  -> __)
+                )
+        );
+    }
+
+    @Test
+    public void testFlowsWithATestSourceANDaTestSink() {
+        Flow<Integer, Integer, NotUsed> flowUnderTest = Flow.<Integer>create().map(__ -> __ * 2);
+
+        Source<Integer, TestPublisher.Probe<Integer>> testSource = TestSource.probe(system);
+        Sink<Integer, TestSubscriber.Probe<Integer>> testSink = TestSink.probe(system);
+
+        Pair<TestPublisher.Probe<Integer>, Probe<Integer>> materialized = testSource.via(flowUnderTest).toMat(testSink, Keep.both()).run(mat);
+
+        materialized.first()
+                .sendNext(1)
+                .sendNext(5)
+                .sendNext(42)
+                .sendNext(99)
+                .sendComplete();
+
+        materialized.second().request(4); // don't forget this!
+        materialized.second()
+                .expectNext(2, 10, 84, 198)
+                .expectComplete();
     }
 }
